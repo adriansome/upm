f12e9577748987b52d87420830acc2350d42a9fe
Matt <matt@probook4525.(none)>
2013-08-28 02:06:56 +0100

    User management view updates.

--- protected/modules/user/views/management/_form.php
+++ protected/modules/user/views/management/_form.php
@@ -2,6 +2,22 @@
 /* @var $this ManagmentController */
 /* @var $model User */
 /* @var $form CActiveForm */
+
+Yii::app()->clientScript->registerCss('user-management-form','
+	div#content span#User_permissions input {
+	    margin-left: 1em;
+	}
+
+	div#content span#User_permissions input:first-of-type {
+	    margin-left: 0;
+	}
+	 
+	div#content span#User_permissions label,
+	div#content span#User_permissions input {
+	    display: inline-block;
+	}
+');
+
 ?>
 
 <div class="form">
@@ -16,24 +32,6 @@
 	<?php echo $form->errorSummary($model); ?>
 
 	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>140)); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'permissions'); ?>
-		<?php echo $form->textField($model,'permissions',array('size'=>6,'maxlength'=>6)); ?>
-		<?php echo $form->error($model,'permissions'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'username'); ?>
-		<?php echo $form->textField($model,'username',array('size'=>40,'maxlength'=>40)); ?>
-		<?php echo $form->error($model,'username'); ?>
-	</div>
-
-	<div class="row">
 		<?php echo $form->labelEx($model,'firstname'); ?>
 		<?php echo $form->textField($model,'firstname',array('size'=>60,'maxlength'=>140)); ?>
 		<?php echo $form->error($model,'firstname'); ?>
@@ -43,6 +41,31 @@
 		<?php echo $form->labelEx($model,'lastname'); ?>
 		<?php echo $form->textField($model,'lastname',array('size'=>60,'maxlength'=>140)); ?>
 		<?php echo $form->error($model,'lastname'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>140)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>40,'maxlength'=>40)); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'permissions'); ?>
+		<?php echo $form->radioButtonList($model,'permissions',
+			array(
+				'member'=>'Member', 'user'=>'User', 'editor'=>'Editor', 'admin'=>'Admin'
+			),
+			array(
+				'separator'=>'','template' => '{input} {label}',
+			)
+		); ?>
+		<?php echo $form->error($model,'permissions'); ?>
 	</div>
 
 	<div class="row buttons">
