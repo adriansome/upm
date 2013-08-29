e5726a86545ae9cf0a698f0f82be12ef9390017c
Matt <matt@probook4525.(none)>
2013-08-29 10:50:53 +0100

    Added module assets directory and listing search javascript file.

---
+++ protected/modules/page/assets/js/search.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$('input#Page_searchTerm').keyup(function(){
+	  submitSearch();
+	  return false;
+	});
+
+	function submitSearch(){
+		$.fn.yiiListView.update('page-list', {
+			data: $('input#User_searchTerm').serialize(),
+		});
+	}
+});
