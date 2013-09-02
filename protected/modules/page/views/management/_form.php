9aef982c7606c147e70c8a66adf4e577279e4ed7
Matt <matt@probook4525.(none)>
2013-09-02 10:46:47 +0100

    Updated to page management views. Unfinished.

--- protected/modules/page/views/management/_form.php
+++ protected/modules/page/views/management/_form.php
@@ -17,44 +17,6 @@
     </div><!-- row -->
 
     <div class="row">
-        <?php echo $form->labelEx($model, 'link'); ?>
-        <?php echo $form->textField($model, 'link', array('size' => 60)); ?>
-        <?php echo $form->error($model, 'link'); ?>
-        <br/>
-        <p class="hint">
-            Use - OR _ in place of spaces.
-        </p>
-    </div><!-- row -->
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'description'); ?>
-        <?php echo $form->textArea($model, 'description', array('rows' => 6, 'cols' => 50)); ?>
-        <?php echo $form->error($model, 'description'); ?>
-    </div><!-- row -->
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'enabled'); ?>
-        <?php echo $form->checkBox($model, 'enabled'); ?>
-        <?php echo $form->error($model, 'enabled'); ?>
-    </div><!-- row -->
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'role'); ?>
-        <?php
-        echo CHtml::checkBoxList(get_class($model) . '[role]', explode(',', $model->role), $model->roles, array('selected' => 'all'));
-        ?>
-        <?php echo $form->error($model, 'role'); ?>
-    </div><!-- row -->
-
-
-    <div class="row">
-        <?php echo $form->labelEx($model, 'Open in new tab?'); ?>
-        <?php echo CHtml::checkBox('Page[target]', $model->target == '_blank', array('value' => '_blank')); ?>
-        <?php echo $form->error($model, 'target'); ?>
-    </div>
-
-
-    <div class="row">
         <?php echo $form->labelEx($model, 'parent_id'); ?>
         <?php
         //show all menu items but current one
@@ -68,10 +30,75 @@
         <?php echo $form->error($model, 'parent_id'); ?>
     </div><!-- row -->
 
+        <div class="row">
+        <?php echo $form->labelEx($model, 'layout'); ?>
+        <?php echo $form->dropDownList($model, 'layout', $this->pageLayouts, array('empty'=>' - ')); ?>
+        <?php echo $form->error($model, 'layout'); ?>
+    </div>
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'link'); ?>
+        <?php echo $form->textField($model, 'link', array('size' => 60)); ?>
+        <?php echo $form->error($model, 'link'); ?>
+        <br/>
+        <p class="hint">
+            Please use either a dash ( - ) or an underscore ( _ ) in place of spaces.
+        </p>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'role'); ?>
+        <?php echo $form->radioButtonList($model,'role',
+            array(
+                'all'=>'All', 'guest'=>'Guest', 'subscriber'=>'Subscriber', 'user'=>'User', 'editor'=>'Editor', 'admin'=>'Admin'
+            ),
+            array(
+                'separator'=>'&nbsp;&nbsp;','template' => '{input} {label}',
+            )
+        ); ?>
+        <?php echo $form->error($model, 'role'); ?>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'meta_description'); ?>
+        <?php echo $form->textArea($model, 'meta_description', array('rows' => 6, 'cols' => 50)); ?>
+        <?php echo $form->error($model, 'meta_description'); ?>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'meta_keywords'); ?>
+        <?php echo $form->textArea($model, 'meta_keywords', array('rows' => 6, 'cols' => 50)); ?>
+        <?php echo $form->error($model, 'meta_keywords'); ?>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'date_active'); ?>
+        <?php echo CHtml::checkBox('Page[active]', !empty($model->date_active), array('value'=>true)) ; ?>
+        <?php echo $form->error($model, 'date_active'); ?>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'date_visible'); ?>
+        <?php echo CHtml::checkBox('Page[visible]', !empty($model->date_visible), array('value'=>true)) ; ?>
+        <?php echo $form->error($model, 'date_visible'); ?>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'date_subpages'); ?>
+        <?php echo CHtml::checkBox('Page[allowSubpages]', !empty($model->dateActive), array('value'=>true)) ; ?>
+        <?php echo $form->error($model, 'date_subpages'); ?>
+    </div><!-- row -->
+
+    <div class="row">
+        <?php echo $form->labelEx($model, 'Open in new tab'); ?>
+        <?php echo CHtml::checkBox('Page[target]', $model->target == '_blank', array('value' => '_blank')); ?>
+        <?php echo $form->error($model, 'target'); ?>
+    </div>
+
     <div class="row buttons">
         <?php
-        echo CHtml::submitButton(Yii::t('app', 'Save'));
-        echo CHtml::Button(Yii::t('app', 'Cancel'), array(
+        echo CHtml::submitButton('Save');
+        echo CHtml::Button('Cancel', array(
             'submit' => 'javascript:history.go(-1)'));
         ?>
     </div>
--- protected/modules/page/views/management/edit.php
+++ protected/modules/page/views/management/edit.php
@@ -1,17 +1,10 @@
-<?php
-$this->menu = array(
-    array('label' => PageModule::t('Manage Menus'), 'url' => array('/menu')),
-    array('label' => PageModule::t('Create New Menu'), 'url' => array('/menu/menu/create')),
-);
-?>
+<h1>Edit <?php echo $model->name; ?> </h1>
 
-<h1> <?php echo Yii::t('app', 'Edit'); ?> <?php echo $model->name; ?> </h1>
 <?php
 $this->widget('zii.widgets.CMenu', array(
     'items' => array(
-        array('label' => Yii::t('app', 'Delete'), 'url' => '#', 'linkOptions' => array('class' => 'button', 'submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
-        )));
+        array('label' => 'Delete', 'url' => '#', 'linkOptions' => array('class' => 'button', 'submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
+)));
 
-$this->renderPartial('_form', array(
-    'model' => $model));
+$this->renderPartial('_form', array('model' => $model));
 ?>
