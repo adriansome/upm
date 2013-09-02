016ce16ef3b4996b9d51b8fd253edca42d1b527f
Matt <matt@probook4525.(none)>
2013-09-02 10:41:56 +0100

    Added example 2col template to demo theme.

---
+++ themes/new_fridge/views/layouts/2col.php
@@ -0,0 +1,13 @@
+<?php $this->layout = '//layouts/righty'; ?>
+
+<?php $this->beginClip('content'); ?>
+   <h1><?php echo $model->name; ?></h1>
+   <p>
+		<?php echo $model->meta_description; ?>
+   </p>
+<?php $this->endClip(); ?>
+?>
+
+<?php $this->beginClip('sidebar1'); ?>
+   <p>2col Side bar content</p>
+<?php $this->endClip(); ?>
