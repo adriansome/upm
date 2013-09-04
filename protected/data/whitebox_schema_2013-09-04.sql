f663c4bd3a5ae51ac8d3418ce36558cb1e530fda
Matt <matt@probook4525.(none)>
2013-09-04 14:04:46 +0100

    Adding most recent database schema and test data dumps.

---
+++ protected/data/whitebox_schema_2013-09-04.sql
@@ -0,0 +1,127 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Host: 33.33.33.33    Database: fanatic
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `old_link`
+--
+
+DROP TABLE IF EXISTS `old_link`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `old_link` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `page_id` int(10) unsigned NOT NULL,
+  `link` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `page_id` (`page_id`) USING BTREE,
+  CONSTRAINT `redirect` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `page`
+--
+
+DROP TABLE IF EXISTS `page`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `page` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) unsigned DEFAULT NULL,
+  `depth` int(11) NOT NULL DEFAULT '1',
+  `lft` int(11) NOT NULL,
+  `rgt` int(11) NOT NULL,
+  `name` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `target` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `window_title` text COLLATE utf8_unicode_ci NOT NULL,
+  `meta_description` text COLLATE utf8_unicode_ci,
+  `meta_keywords` text COLLATE utf8_unicode_ci,
+  `link` text COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('all','guest','subscriber','user','editor','admin') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'all',
+  `layout` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `date_created` datetime NOT NULL,
+  `date_updated` datetime DEFAULT NULL,
+  `date_active` datetime DEFAULT NULL,
+  `date_visible` datetime DEFAULT NULL,
+  `date_subpages` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`) USING BTREE,
+  CONSTRAINT `parent` FOREIGN KEY (`parent_id`) REFERENCES `page` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `page_menu`
+--
+
+DROP TABLE IF EXISTS `page_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `page_menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `page_id` int(11) unsigned NOT NULL,
+  `menu_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `page_id` (`page_id`) USING BTREE,
+  CONSTRAINT `page` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
+  `oldEmail` varchar(140) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('subscriber','user','editor','admin') COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `firstname` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
+  `lastname` varchar(140) COLLATE utf8_unicode_ci NOT NULL,
+  `dateTermsAgreed` datetime NOT NULL,
+  `dateUpdated` datetime DEFAULT NULL,
+  `dateLastLogin` datetime DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateValidationEmailSent` datetime DEFAULT NULL,
+  `activationCode` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dateEmailValidated` datetime DEFAULT NULL,
+  `dateAccountExpire` datetime DEFAULT NULL,
+  `dateRevert` datetime DEFAULT NULL,
+  `dateReset` datetime DEFAULT NULL,
+  `dateDeleted` datetime DEFAULT NULL,
+  `resetCode` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `revertCode` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-09-04 14:01:45
