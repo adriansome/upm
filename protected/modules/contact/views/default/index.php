9a0dcccbe40c17ce3c2eeaac4e1f670f50c41f05
Matt <matt@probook4525.(none)>
2013-09-04 11:16:36 +0100

    Bug Fix: Added main content clip wrapper to contact form view.

--- protected/modules/contact/views/default/index.php
+++ protected/modules/contact/views/default/index.php
@@ -6,8 +6,12 @@ $this->pageTitle='Contact Us';
 $this->breadcrumbs=array(
 	'Contact Us',
 );
+
+$this->beginClip('content');
 ?>
 
 <h1>Contact Us</h1>
 
 <?php echo $this->renderPartial('_form', array('model'=>$model)); ?>
+
+<?php $this->endClip(); ?>
