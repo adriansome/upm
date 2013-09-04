4a03e7863c016e39d051008a9e0fb8b0612c23c7
Matt <matt@probook4525.(none)>
2013-09-04 11:34:20 +0100

    Added active menu item checks for testing purposes.

--- themes/new_fridge/views/layouts/body.php
+++ themes/new_fridge/views/layouts/body.php
@@ -8,14 +8,15 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'Contact', 'url'=>array('/contact')),
-				array('label'=>'Register', 'url'=>array('/user/register'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'User Profile', 'url'=>array('/user/profile'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Contact', 'url'=>array('/contact'), 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='contact')),
+				array('label'=>'Register', 'url'=>array('/user/register'), 'visible'=>Yii::app()->user->isGuest, 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='user' && Yii::app()->controller->action->id=='register')),
+				array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest, 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='user' && Yii::app()->controller->action->id=='login')),
+				array('label'=>'User Profile', 'url'=>array('/user/profile'), 'visible'=>!Yii::app()->user->isGuest, 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='user' && Yii::app()->controller->action->id=='profile')),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/user/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
 	</div><!-- mainmenu -->
+	
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
