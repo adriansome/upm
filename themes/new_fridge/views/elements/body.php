9beb0b71ad1fdb4d6864f4d2500bc20f61c8b48d
Matt <matt@probook4525.(none)>
2013-09-09 21:59:14 +0100

    Demo theme updates: now using menu widget.

--- themes/new_fridge/views/elements/body.php
+++ themes/new_fridge/views/elements/body.php
@@ -4,18 +4,9 @@
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/home'), 'active'=>($_SERVER['REQUEST_URI'] == '/home')),
-				array('label'=>'Contact', 'url'=>array('/contact'), 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='contact')),
-				array('label'=>'Register', 'url'=>array('/user/register'), 'visible'=>Yii::app()->user->isGuest, 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='user' && Yii::app()->controller->action->id=='register')),
-				array('label'=>'Login', 'url'=>array('/user/login'), 'visible'=>Yii::app()->user->isGuest, 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='user' && Yii::app()->controller->action->id=='login')),
-				array('label'=>'User Profile', 'url'=>array('/user/profile'), 'visible'=>!Yii::app()->user->isGuest, 'active'=>(isset(Yii::app()->controller->module) && Yii::app()->controller->module->id=='user' && Yii::app()->controller->action->id=='profile')),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/user/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
+	<?php $this->widget('Menu',array(
+		'id'=>'mainmenu'
+	)); ?><!-- mainmenu -->
 	
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
