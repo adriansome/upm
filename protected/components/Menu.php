b942beca84bd173148fb245683f92d509057a9ec
Sam Ramsay <sam_ramsay@yahoo.com>
2013-09-16 18:30:21 +0100

    Nested menus display and basic styles

--- protected/components/Menu.php
+++ protected/components/Menu.php
@@ -20,17 +20,24 @@ class Menu extends CMenu
 			}
 
 			// Find all the active/visible pages that belong to this menu.
-			$sql  = 'SELECT page.name AS label, page.link AS url, page.role ';
-			$sql .= 'FROM page ';
-			$sql .= 'LEFT JOIN page_menu ON page.id = page_menu.page_id ';
-			$sql .= 'WHERE page.date_active IS NOT NULL ';
-			$sql .= 'AND page.date_visible IS NOT NULL ';
-			$sql .= 'AND page_menu.menu_id = :menu_id ';
-			$sql .= 'ORDER BY page.lft ASC';
-
-			$command = Yii::app()->db->createCommand($sql);
-			$command->params = array(':menu_id' => $_id);
+			$command = Yii::app()->db->createCommand()
+				->select('lv1.id AS lv1ID, lv2.id AS lv2ID, lv3.id AS lv3ID, lv1.name AS label, lv1.link AS url, lv1.role AS role, lv2.name AS lv2Label, lv2.link AS lv2Url, lv2.role AS lv2Role, lv3.name AS lv3Label, lv3.link AS lv3Url, lv3.role AS lv3Role')
+				->from('page lv1')
+				->join('page_menu menu', 'lv1.id = menu.page_id')
+				    
+				->leftJoin('page lv2', 'lv1.id = lv2.parent_id')
+				->leftJoin('page lv3', 'lv2.id = lv3.parent_id')
+				    
+				->where('lv1.date_active IS NOT NULL AND lv1.date_visible IS NOT NULL')
+				->where('((lv2.date_active IS NOT NULL AND lv2.date_visible IS NOT NULL) OR lv2.name IS NULL)')
+				->where('((lv3.date_active IS NOT NULL AND lv3.date_visible IS NOT NULL) OR lv3.name IS NULL)')
+				->where('menu.menu_id = :menu_id', array(':menu_id' => $_id))
+				    
+				->order('lv1.lft ASC, lv2.lft ASC, lv3.lft ASC')
+			;
 			$pages = $command->queryAll();
+			
+			$menu_pages = array();
 
 			// If pages were found for this menu then
 			if(is_array($pages))
@@ -42,12 +49,36 @@ class Menu extends CMenu
 					if($page_uri[0] != '/')
 						$page_uri = '/'.$page_uri;
 
-					$pages[$index]['active'] = ($_SERVER['REQUEST_URI'] == $page_uri);
-					$pages[$index]['visible'] = ($page['role'] == 'all' || (Yii::app()->user->isGuest && $page['role'] == 'guest') || ((!Yii::app()->user->isGuest && Yii::app()->user->role == $page['role']) || (($page['role'] == 'user' || $page['role'] == 'subscriber') && Yii::app()->user->isAdmin())));
-					$pages[$index]['url'] = Yii::app()->getBaseUrl(true).$page_uri;
+					$menu_pages[$page['lv1ID']]['label'] = $page['label'];
+					$menu_pages[$page['lv1ID']]['active'] = ($_SERVER['REQUEST_URI'] == $page_uri);
+					$menu_pages[$page['lv1ID']]['visible'] = ($page['role'] == 'all' || (Yii::app()->user->isGuest && $page['role'] == 'guest') || ((!Yii::app()->user->isGuest && Yii::app()->user->role == $page['role']) || (($page['role'] == 'user' || $page['role'] == 'subscriber') && Yii::app()->user->isAdmin())));
+					$menu_pages[$page['lv1ID']]['url'] = Yii::app()->getBaseUrl(true).$page_uri;
+					     
+					if($page['lv2ID']){
+						$page_uri = $page['lv2Url'];
+					      
+						if($page_uri[0] != '/')
+							$page_uri = '/'.$page_uri;
+						$menu_pages[$page['lv1ID']]['items'][$page['lv2ID']] = array('label'=>$page['lv2Label'],
+							'active'=>($_SERVER['REQUEST_URI'] == $page_uri),
+							'visible'=>($page['lv2Role'] == 'all' || (Yii::app()->user->isGuest && $page['lv2Role'] == 'guest') || ((!Yii::app()->user->isGuest && Yii::app()->user->role == $page['lv2Role']) || (($page['lv2Role'] == 'user' || $page['lv2Role'] == 'subscriber') && Yii::app()->user->isAdmin()))),
+							'url'=>Yii::app()->getBaseUrl(true).$page_uri
+							);
+						if($page['lv3ID']){
+							$page_uri = $page['lv3Url'];
+	
+							if($page_uri[0] != '/')
+								$page_uri = '/'.$page_uri;
+							$menu_pages[$page['lv1ID']]['items'][$page['lv2ID']]['items'][$page['lv3ID']] = array('label'=>$page['lv3Label'],
+								'active'=>($_SERVER['REQUEST_URI'] == $page_uri),
+								'visible'=>($page['lv3Role'] == 'all' || (Yii::app()->user->isGuest && $page['lv3Role'] == 'guest') || ((!Yii::app()->user->isGuest && Yii::app()->user->role == $page['lv3Role']) || (($page['lv3Role'] == 'user' || $page['lv3Role'] == 'subscriber') && Yii::app()->user->isAdmin()))),
+								'url'=>Yii::app()->getBaseUrl(true).$page_uri
+								);
+						}
+					}
 				}
 				
-				$this->items = array_merge($pages, $this->items);
+				$this->items = array_merge($menu_pages, $this->items);
 			}	
 		}
 
