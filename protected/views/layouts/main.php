5a697b1d887f71d613eeeac91530ec76fb665e0b
Matt <matt@probook4525.(none)>
2013-09-19 17:37:39 +0100

    Reverting changes to bootstrap registration as not registering css breaks the modals.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
 <head>
-	<?php Yii::app()->bootstrap->registerCoreScripts(); ?>
+	<?php Yii::app()->bootstrap->register(); ?>
 	<?php require_once(Yii::app()->theme->basepath.'/views/elements/head.php'); ?>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('bootstrap')); ?>/assets/css/bootstrap-wysihtml5.css"></link>
 	<script src="<?php echo Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('bootstrap')); ?>/assets/js/wysihtml5-0.3.0.js"></script>
