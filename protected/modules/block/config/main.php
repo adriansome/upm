af8ddf96429f4e5ebc51a8c039a63a6b1ae8a404
Matt <matt@probook4525.(none)>
2013-09-25 10:23:45 +0100

    Temp fix for non-loading modules bug.

--- protected/modules/block/config/main.php
+++ protected/modules/block/config/main.php
@@ -2,19 +2,19 @@
 $module_name = basename(dirname(dirname(__FILE__)));
 $default_controller = false;
 
-$listsDir = dirname(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR.'themes'.DIRECTORY_SEPARATOR.$config['theme'].DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'lists'.DIRECTORY_SEPARATOR;
-$handle = opendir($listsDir);
-$listManagementActions = array();
-$lists = array();
+// $listsDir = dirname(dirname(dirname(dirname(dirname(__FILE__))))).DIRECTORY_SEPARATOR.'themes'.DIRECTORY_SEPARATOR.$config['theme'].DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'lists'.DIRECTORY_SEPARATOR;
+// $handle = opendir($listsDir);
+// $listManagementActions = array();
+// $lists = array();
 
-while(false !== ($file = readdir($handle)))
-{
-    if ($file != "." && $file != ".." && is_dir($listsDir . $file))
-    {
-        $listManagementActions[ucfirst($file).' Management'] = array('/'.$file.'/management');
-        $lists[] = $file;
-    }
-}
+// while(false !== ($file = readdir($handle)))
+// {
+//     if ($file != "." && $file != ".." && is_dir($listsDir . $file))
+//     {
+//         $listManagementActions[ucfirst($file).' Management'] = array('/'.$file.'/management');
+//         $lists[] = $file;
+//     }
+// }
  
 return array(
     'import' => array(
@@ -30,14 +30,14 @@ return array(
     'components' => array(
         'urlManager' => array(
             'rules' => array(
-                '<list:('.implode('|', $lists).')>/management' => $module_name . '/management/list',
+                // '<list:('.implode('|', $lists).')>/management' => $module_name . '/management/list',
                 $module_name . '/management/<action:\w+>/<id:\d+>' => $module_name . '/management/<action>',
                 $module_name . '/management/<action:\w+>' => $module_name . '/management/<action>',
             ),
         ),
     ),
 
-    'params'=>array(
-        'managementActions'=>$listManagementActions,
-    ),
+    // 'params'=>array(
+    //     'managementActions'=>$listManagementActions,
+    // ),
 );
