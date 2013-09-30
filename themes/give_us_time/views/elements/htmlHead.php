c52dd2d360d13c40b2b7629b6fc98e7774fd6669
Matt <matt@probook4525.(none)>
2013-09-30 14:09:55 +0100

    Give us time theme integration.

--- themes/give_us_time/views/elements/htmlHead.php
+++ themes/give_us_time/views/elements/htmlHead.php
@@ -2,6 +2,8 @@
 <meta name="language" content="en" />
 <meta name="description" content="" />
 <meta name="keywords" content="" />
-<title><?php echo CHtml::encode($this->pageTitle); ?> | <?php echo Yii::app()->name; ?></title>
+
 <link rel="icon" href="images/favicon.ico" type="image/x-icon" />
-<link rel="stylesheet" href="css/styles.css" />
+<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
+
+<title><?php echo CHtml::encode($this->pageTitle); ?> | <?php echo Yii::app()->name; ?></title>
---
+++ themes/give_us_time/views/templates/home-old.php
@@ -0,0 +1,57 @@
+<?php $this->pageTitle = $model->window_title; ?>
+
+<?php require_once(Yii::app()->theme->basepath.'/views/elements/header.php'); ?>
+
+<div id="content">
+<h1>
+	<?php $this->widget('SingleLineText',array(
+		'name'=>'test text',
+		'scope'=>'page',
+	)); ?>
+</h1>
+
+<p>
+	<?php $this->widget('RichText',array(
+		'name'=>'test text block',
+		'scope'=>'page',
+	)); ?>
+</p>
+
+<h2>
+	<?php $this->widget('SingleLineText',array(
+		'name'=>'test heading',
+		'scope'=>'page',
+	)); ?>
+</h2>
+
+<p>
+	<?php $this->widget('MultiLineText',array(
+		'name'=>'test text block 2',
+		'scope'=>'page',
+	)); ?>
+</p>
+
+<?php $this->widget('Image',array(
+	'name'=>'test image block',
+	'scope'=>'page',
+)); ?>
+
+<?php $this->widget('NuggetArea',array(
+	'name'=>'home nugget area',
+)); ?>
+
+<?php $this->widget('ListWidget',array(
+	'name'=>'blog',
+	'scenario'=>'list',
+	'pageSize'=>2,
+)); ?>
+
+<?php $this->widget('Carousel',array(
+	'name'=>'home-carousel',
+	'options'=>array(
+
+	),
+)); ?>
+</div>
+
+<?php require_once(Yii::app()->theme->basepath.'/views/elements/footer.php'); ?>
