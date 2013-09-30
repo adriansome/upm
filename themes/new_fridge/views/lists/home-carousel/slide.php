d8cbbc8d97f2685eaa8976aa36eaab51ca5b1503
Matt <matt@probook4525.(none)>
2013-09-30 15:23:25 +0100

    Carousel views updated for demo theme.

--- themes/new_fridge/views/lists/home-carousel/slide.php
+++ themes/new_fridge/views/lists/home-carousel/slide.php
@@ -1,8 +1,8 @@
-<div class="girdle">
+<li>
 	<div class="summary-text">
 		<h2><?php echo $data['title']; ?></h2>
 		<p><?php echo $data['text']; ?></p>
 		<a href="<?php echo $data['link_href']; ?>" title="<?php echo $data['link_title']; ?>"><?php echo $data['link_text']; ?></a>
 	</div>
 	<img src="<?php echo $data['image_src']; ?>" alt="<?php echo $data['image_alt']; ?>" />
-</div>
+</li>
