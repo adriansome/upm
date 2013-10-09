999a7d598e0273c79ad17dcc5f7a1aa9201f26bd
Jack Stowey <jackstowey@gmail.com>
2013-10-09 11:54:21 +0100

    Fixed a bug with the registration success page and it wiping out the referer part of the session

--- protected/modules/user/UserModule.php
+++ protected/modules/user/UserModule.php
@@ -12,8 +12,9 @@ class UserModule extends CWebModule
 	{
 		if(parent::beforeControllerAction($controller, $action))
 		{
-			if(($action->id != 'registrationSuccess' && $action->id != 'resendEmailVerification') && isset(Yii::app()->session['referer']) && (Yii::app()->session['referer']['action'] == 'registrationSuccess' || Yii::app()->session['referer']['action'] == 'sendEmailVerification'))
+			if(($action->id != 'registrationsuccess' && $action->id != 'resendEmailVerification') && isset(Yii::app()->session['referer']) && (Yii::app()->session['referer']['action'] == 'registrationSuccess' || Yii::app()->session['referer']['action'] == 'sendEmailVerification')) {
 				unset(Yii::app()->session['referer']);
+			}
 			
 			return true;
 		}
