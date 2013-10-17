c48a7bd80b84e83cda75ad68d586baf8a99c9f8b
Jack Stowey <jackstowey@gmail.com>
2013-10-17 11:35:18 +0100

    Markup changes to news items

--- themes/give_us_time/views/lists/news/list.php
+++ themes/give_us_time/views/lists/news/list.php
@@ -1,17 +1,14 @@
 <?php /* @var $this ListWidget */ ?>
+<h1>News</h1>
 
 <?php
 $this->widget('zii.widgets.CListView', array(
-	'id'=>$this->id,
-	'dataProvider'=>$this->contents,
-	'itemView'=>'summary',
-	'htmlOptions' => array(
-		'class' => 'constrained'					   
-	),
-	'summaryText'=> 'News',
-	'template'=>'{summary} {items} {pager}',
-	'pager'=>array(
-            'class'=>'CLinkPager',
-            'header'=>'',
-   	),
+    'id'=>$this->id,
+    'dataProvider'=>$this->contents,
+    'itemView'=>'summary',
+    'template'=>'{items} {pager}',
+    'pager'=>array(
+        'class'=>'CLinkPager',
+        'header'=>'',
+    ),
 )); ?>
--- themes/give_us_time/views/lists/news/summary.php
+++ themes/give_us_time/views/lists/news/summary.php
@@ -1,33 +1,30 @@
 <?php /* @var $data CArrayDataProvider */ ?>
 
-<div class="news-item">
-	<div class="image">
-	<?php
-	$fullwidth = TRUE;
-	if (isset($data['full_image']) && !empty($data['full_image'])) {
-		$img_src = $data['full_image'];
-		$img_path = Yii::app()->basePath . '/..' . $img_src;
-		if (is_file($img_path)) {
-			echo "<img src='{$img_src}' />";
-			$fullwidth = FALSE;
-		}
-	}
+<article class="news-item">
+    
+    <div class="image column span4">
+    <?php
+    if (isset($data['full_image']) && !empty($data['full_image'])) {
+        $img_src = $data['full_image'];
+        $img_path = Yii::app()->basePath . '/..' . $img_src;
+        if (is_file($img_path)) {
+            echo "<img alt='' src='{$img_src}' />";
+        }
+    }
 
-	?>
-	</div>
-	<div class='text<?php echo ($fullwidth) ? ' fullwidth' : ''; ?>'>
-		<div class='inner'>
-			<div class='headline'>
-				<?php echo (isset($data['short_headline']) && !empty($data['short_headline']))
-				? $data['short_headline'] : $data['title']; ?>
-				<div class='read-more'><a href='?slug=<?php echo $data['slug']; ?>'>Read more</a></div>
-			</div>
-			<div class='summary'>
-				<p><?php echo Yii::app()->utility->truncate_text($data['summary'], 200); ?></p>	
-			</div>
-		</div>
-	</div>
-
+    ?>
+    </div>
+    <div class='text column span12'>
+        <div class='inner'>
+            <div class='headline-wrapper'>
+                    <?php echo (isset($data['short_headline']) && !empty($data['short_headline']))
+                    ? $data['short_headline'] : $data['title']; ?>
+            </div>
+            <div class='summary'>
+                <p><?php echo Yii::app()->utility->truncate_text($data['summary'], 200); ?></p>	
+                <div class='more'><a href='?slug=<?php echo $data['slug']; ?>'>Read more</a></div>
+            </div>
+        </div>
+    </div>
 	
-	
-</div>
+</article>
