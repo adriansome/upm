ef775747546585cc1e0f2684cabaa6a53d0333e0
Jack Stowey <jackstowey@gmail.com>
2013-10-17 11:01:33 +0100

    Detail view of news now outputs an image

--- themes/give_us_time/views/lists/news/detail.php
+++ themes/give_us_time/views/lists/news/detail.php
@@ -3,7 +3,17 @@ $data = $this->contents->rawData;
 ?>
 
 <div class="news-item">
-	<h1><?php echo $data['title']; ?></h1>
+    
+    <h1><?php echo $data['title']; ?></h1>
+    <?php
+    if (isset($data['full_image']) && !empty($data['full_image'])) {
+            $img_src = $data['full_image'];
+            $img_path = Yii::app()->basePath . '/..' . $img_src;
+            if (is_file($img_path)) {
+                echo "<img src='{$img_src}' />";
+            }
+    }
+    ?>
     <div class="story">
         <?php echo $data['full_story']; ?>
     </div>
