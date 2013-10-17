6ffdf10f58d62150fd7ebdd4f70478afc77337df
Rob <rob@fanaticdesign.co.uk>
2013-10-17 11:44:23 +0100

    Favicon & admin styles

--- themes/give_us_time/views/elements/htmlHead.php
+++ themes/give_us_time/views/elements/htmlHead.php
@@ -3,7 +3,7 @@
 <meta name="description" content="" />
 <meta name="keywords" content="" />
 
-<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="<?php echo Yii::app()->theme->baseUrl; ?>/images/favicon.ico" type="image/x-icon" />
 <link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
 
 <title><?php echo CHtml::encode($this->pageTitle); ?> | <?php echo Yii::app()->name; ?></title>
