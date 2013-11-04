9a128ab8b222acb1065567e1a469b3c08f4bf758
Jo <jo@cloudyvision.co.uk>
2013-11-04 16:59:00 +0000

    Return to homepage added to end of registration for user

--- themes/give_us_time/views/register/success.php
+++ themes/give_us_time/views/register/success.php
@@ -5,6 +5,8 @@ if ($type == 'user') {
 <p><strong>Thank you for registering with Give Us Time</strong></p>
 <p>Verifying your details may take up to 48 hours. Once verified, we'll notify you by email and send you a link to the Give Us Time holiday search page.</p>
 
+<a href="/">Return to homepage</a>
+
 <?php
 } else if ($type == 'landlord') {
 ?>
