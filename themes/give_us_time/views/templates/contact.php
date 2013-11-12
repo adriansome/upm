bbf7e78cf2b19f918b4548d3b0e0479977280f11
Jack Stowey <jackstowey@gmail.com>
2013-11-12 10:31:13 +0000

    Added contact form functionality

--- themes/give_us_time/views/templates/contact.php
+++ themes/give_us_time/views/templates/contact.php
@@ -1,3 +1,41 @@
+<?php
+// Should get this from config, but quick fix for now
+$recipient = 'enquiries@giveustime.org.uk';
+$errors = array();
+
+// Send the contact form
+if (isset($_POST['contact-send']) && $_POST['contact-send']) {
+    
+    if(isset($_POST['name'], $_POST['email'], $_POST['message'])) {
+        if ($_POST['name'] && $_POST['email'] && $_POST['message']) {
+            if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                $errors['email'] = 'Please enter a valid email address.';
+            }
+            $email = new YiiMailMessage;
+            $email->setBody($_POST['message'], 'text/html');
+            $email->setSubject('Contact Form - ' . Yii::app()->name);
+            $email->addTo($recipient);
+            $email->setFrom(array($_POST['email'] => $_POST['name']));
+            Yii::app()->mail->send($email);
+        } else {
+            if (!$_POST['name']) {
+                $errors['name'] = 'This field is required.';
+            }
+            if (!$_POST['email']) {
+                $errors['email'] = 'This field is required.';
+            }
+            if (!$_POST['message']) {
+                $errors['message'] = 'This field is required.';
+            }
+        }
+    } else {
+        $errors[] = 'Missing required fields';
+    }
+    
+}
+
+?>
+
 <?php $this->pageTitle = $model->window_title; ?>
 
 <?php require_once(Yii::app()->theme->basepath.'/views/elements/header.php'); ?>
@@ -43,21 +81,41 @@
 				<form action="#" method="post" class="standard-form" id="contact-form">
 					<div class="form-row">
 						<label for="contact-name">Name <span class="required">(required)</span></label>
-						<input  id="contact-name" type="text" />
-						<p class="validation error">Please enter a name</p>
+                        <input name="name" id="contact-name" value="<?php if (isset($_POST['name'])) { echo  htmlentities($_POST['name']); } ?>" type="text" />
+                        <?php
+                        if (isset($errors['name'])):
+                        ?>
+						<p class="validation error"><?php echo $errors['name'] ?></p>
+                        <?php
+                        endif;
+                        ?>
 					</div>
 					<div class="form-row">
 						<label for="contact-email">E-mail <span class="required">(required)</span></label>
-						<input  id="contact-email" type="text" />
-						<p class="validation error">Please enter a valid e-mail address</p>
+						<input name="email" value="<?php if (isset($_POST['email'])) { echo  htmlentities($_POST['email']); } ?>"  id="contact-email" type="text" />
+                        <?php
+                        if (isset($errors['email'])):
+                        ?>
+						<p class="validation error"><?php echo $errors['email'] ?></p>
+                        <?php
+                        endif;
+                        ?>
 					</div>
 					<div class="form-row">
-						<label for="contact-message">Name <span class="required">(required)</span></label>
-						<textarea id="contact-message" cols="20" rows="6"></textarea>
-						<p class="validation error">Please enter a message</p>
+						<label for="contact-message">Message <span class="required">(required)</span></label>
+                        <textarea name="message" id="contact-message" cols="20" rows="6"><?php if (isset($_POST['message'])) { 
+                            echo htmlentities($_POST['message']);
+                        } ?></textarea>
+                        <?php
+                        if (isset($errors['message'])):
+                        ?>
+						<p class="validation error"><?php echo $errors['message'] ?></p>
+                        <?php
+                        endif;
+                        ?>
 					</div>
 					<div class="form-row button-row">
-						<input type="submit" value="Send" />
+						<input name="contact-send" type="submit" value="Send" />
 					</div>
 				</form>
 
