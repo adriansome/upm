597d50eb0bd9427b38262bea68785f5317bbc3c4
ian Collis-Smith <ian@fanaticdesign.co.uk>
2013-11-27 11:19:42 +0100

    Ian editing the email template

--- themes/give_us_time/views/emails/email-validation/view.php
+++ themes/give_us_time/views/emails/email-validation/view.php
@@ -1,15 +1,15 @@
-<h3>Hi <?php echo $params['fullname']; ?>,</h3>
+<p>Hi <?php echo $params['fullname']; ?>,</p>
 
 <?php
 if (isset($header)) {
-    echo "<p>$header</p>";
+    echo "<p style="margin:20px 0 20px 0">$header</p>";
 }?>
 
 <?php $url = $this->createAbsoluteUrl('/user/validate?uid=' . $params['uid']) ?>
-<h3>Welcome and thank you for registering to use <?php echo Yii::app()->name ?></h3><br />
-<p><?php echo CHtml::link('Please follow this link to activate your new user account on<br/>' .  Yii::app()->name, $url); ?></p>
+
+<p><?php echo CHtml::link('' .  Yii::app()->name, $url); ?></p>
 
 <?php if (isset($footer)) {
-    echo "<p>$footer</p>";
+    echo "<p style="margin:20px 0 20px 0" >$footer</p>";
 }
 ?>
